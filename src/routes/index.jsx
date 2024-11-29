b782e0cd3afd7856223494366a8fb88f9dc1e7b7
Muliminty <313195303@qq.com>
2024-11-29 02:46:10 +0000

    在 React 中使用 GridStack.js

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import Home from '@/page/home';
 import About from '@/page/about';
 import interactive_case from './interactive-case';
+import DraggableResizableGrid from '@/page/draggable-resizable-grid';
 const routes = [
   {
     path: '/',
@@ -10,6 +11,11 @@ const routes = [
     title: '关于',
     path: '/about',
     component: About,
+  },
+  {
+    title: '可拖拽可缩放看板',
+    path: '/draggableResizableGrid',
+    component: DraggableResizableGrid,
   },
   ...interactive_case,
 ];
