e705852d3bc97803440c6e9639c04a074993dea4
Muliminty <313195303@qq.com>
2024-12-26 07:44:04 +0000

    动态看板

--- src/page/idea/card/index.jsx
+++ src/page/idea/card/index.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 import DayNight from './dayNight'
 import styles from './card.module.scss'
+import ControlledStack from './ControlledStack'
 
 const CardContent = () => {
+    const [items, setItems] = useState([
+        {
+            id: 'item-1',
+            component: <DayNight />,
+            width: 3, height: 3, // 默认宽高
+            maxWidth: 4, minWidth: 2, maxHeight: 4, minHeight: 2 // 最大最小宽高
+        },
+        {
+            id: 'item-2',
+            component: '333',
+            width: 2, height: 2, // 默认宽高
+            maxWidth: 3, minWidth: 2, maxHeight: 3, minHeight: 2 // 最大最小宽高
+        }
+    ]);
+
     return (
         <>
             <h2>创意小卡片</h2>
@@ -9,6 +26,11 @@ const CardContent = () => {
                 <div style={{ width: '300px', height: '300px' }}>
                     <DayNight />
                 </div>
+
+                <ControlledStack
+                    items={items}
+                    addItem={() => setItems([...items, { id: `item-${items.length + 1}`, width: 2, height: 2, maxWidth: 3, minWidth: 2, maxHeight: 3, minHeight: 2 }])}
+                />
             </div>
         </>
     );
