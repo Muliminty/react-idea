36897232210a19997e6e426252095cfe9e112002
muliminty <313195303@qq.com>
2025-12-19 16:10:44 +0800

    清空

--- src/components/loading/index.jsx
+++
@@ -1,8 +0,0 @@
-import style from './style.module.scss'
-
-const Loading = () => {
-    return <div className={style.loading} ></div >
-}
-
-
-export default Loading
--- src/layout/empty.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-const Empty = () => {
-    return <Outlet />;
-};
-
-export default Empty;
